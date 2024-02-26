1a189a1a67fcad786f7b277eaff5d806825de05f
Pedro Rau <pedroraudelacruz@gmail.com>
2024-02-26 13:05:21 -0500

    update sdk target

--- app/src/main/java/com/pedrorau/flawlesspoetrade/SplashActivity.kt
+++ app/src/main/java/com/pedrorau/flawlesspoetrade/SplashActivity.kt
@@ -1,7 +1,7 @@
 package com.pedrorau.flawlesspoetrade
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
